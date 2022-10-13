dabdd56d7375db6d6095daadab8fcd7c64061ab5
Sonicfury <theo.lambrt@gmail.com>
2022-10-13 02:34:30 +0200

    updated nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,11 @@
-vim.opt.relativenumber = true
+vim.g.mapleader = ","
+
+local o = vim.opt
+
+o.relativenumber = true
+o.encoding = "UTF-8"
+o.shiftwidth = 4
+o.tabstop= 4
+o.expandtab = true
+
+require("plugins")

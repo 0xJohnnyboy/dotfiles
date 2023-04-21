37b574bb6e2fcf96b4be55db98c3a334de852964
Sonicfury <theo.lambrt@gmail.com>
2023-04-21 12:17:19 +0200

    nvim lsp-zero completions bindings

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,18 +1,42 @@
 local lsp = require('lsp-zero').preset({
-	suggest_lsp_servers = true,
+    suggest_lsp_servers = true,
 })
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 
 lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'lua_ls',
+    'tsserver',
+    'eslint',
+    'lua_ls',
 })
 
 lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+cmp.setup({
+    mapping = {
+        ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                local entry = cmp.get_selected_entry()
+                if not entry then
+                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                else
+                    cmp.confirm()
+                end
+            else
+                fallback()
+            end
+        end, { "i", "s", "c", }),
+        ['<Tab>'] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            end
+        end, { "i", "s", "c", })
+    }
+})
 
 lsp.setup()

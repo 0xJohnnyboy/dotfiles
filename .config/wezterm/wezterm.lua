ebea5c67ec4463d5e69c8acdc8be963d97b65bb0
Sonicfury <theo.lambrt@gmail.com>
2024-01-11 17:35:00 +0100

    wezterm: added bg blur and title bar style

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,6 +11,11 @@ end
 
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.window_background_opacity = 0.8
+config.macos_window_background_blur = 9
+config.win32_system_backdrop = 'Acrylic'
+
+config.window_decorations = "INTEGRATED_BUTTONS"
+
 config.font = wezterm.font_with_fallback {
   'JetBrainsMono NF',
   'Hack Nerd Font Mono',

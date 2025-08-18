fb1be7c41d39dd696561c32ea8ee9721f7ad7ada
0xJohnnyboy <lambert.theo@proton.me>
2025-08-18 17:31:52 +0200

    fix: forgot noice + fixed kulala

--- .config/nvim/lua/plugins/kulala.nvim.lua
+++ .config/nvim/lua/plugins/kulala.nvim.lua
@@ -3,6 +3,7 @@
 
 return {
     "mistweaverco/kulala.nvim",
+    lazy = false,
     keys = {
         {
             "<leader>Rs",
@@ -17,10 +18,21 @@ return {
             desc = "Open scratchpad"
         }
     },
-    ft = {"http", "rest"},
+    ft = { "http", "rest" },
     opts = {
-        global_keymaps = false,
+        global_keymaps = true,
         global_keymaps_prefix = "<leader>R",
-        kulala_keymaps_prefix = ""
+        kulala_keymaps_prefix = "",
+        ui = {
+            autocomplete = true, -- Enable autocomplete suggestions
+            lua_syntax_hl = true, -- Highlight Lua syntax in scripts
+            float = {
+                width = 0.9, -- Width of floating windows (0-1 for percentage)
+                height = 0.8, -- Height of floating windows (0-1 for percentage)
+                border = "rounded", -- Border style for floating windows
+            },
+        },
+        show_icons = "signcolumn", -- Where to show status icons ("signcolumn", "virtual" or false)
     }
 }
+

385b35133a8f3a3ec5cf1cab6393a976a9eca23d
0xJohnnyboy <lambert.theo@proton.me>
2025-10-29 11:28:06 +0100

    fix: untrack tmux plugins and add to gitignore

---
+++ .config/tmux-powerline/config.sh
@@ -0,0 +1,17 @@
+# Tmux-powerline configuration
+# This file configures tmux-powerline to use our custom theme
+
+# Path to the tmux-powerline installation
+export TMUX_POWERLINE_DIR_HOME="${HOME}/.config/tmux/plugins/tmux-powerline"
+
+# Use custom theme
+export TMUX_POWERLINE_THEME="custom"
+
+# Path to custom themes directory
+export TMUX_POWERLINE_DIR_USER_THEMES="${HOME}/.config/tmux-powerline/themes"
+
+# Refresh interval
+export TMUX_POWERLINE_STATUS_INTERVAL="2"
+
+# Status line justification
+export TMUX_POWERLINE_STATUS_JUSTIFICATION="left"

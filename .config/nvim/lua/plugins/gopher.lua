2fa51289e4ce8378cd2a29a81f32e8d13c5bd6bb
0xJohnnyboy <lambert.theo@proton.me>
2026-06-29 15:06:49 +0200

    nvim: gopher.lua

--- .config/nvim/lua/plugins/gopher.lua
+++ .config/nvim/lua/plugins/gopher.lua
@@ -5,5 +5,9 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
     },
+    build = function()
+        vim.system({ "go", "install", "github.com/cweill/gotests/gotests@latest" }):wait()
+        vim.system({ "go", "install", "github.com/koron/iferr@latest" }):wait()
+    end,
     opts = {},
 }
